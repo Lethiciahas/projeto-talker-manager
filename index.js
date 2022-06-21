c1cd0ae9e01abe7379fcda035d1e66bd7c2b35fd
Lethiciahas <lethiciahas@gmail.com>
2022-06-21 14:08:40 -0400

    feat: requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,14 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+// req 01
+app.get('/talker', async (_req, res) => {
+    const getTalker = JSON.parse(await fs.readFile('./talker.json'));
+    res.status(HTTP_OK_STATUS).json(getTalker);
+});
+// req 02
+
+// req 03
 
 app.listen(PORT, () => {
   console.log('Online');
